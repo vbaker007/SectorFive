1baafb3dd5e228f98f69a51cd269fe2020c1aa89
vbaker007 <vbaker007@hotmail.com>
2016-03-22 11:38:41 -0400

    Add Moving Ability to Player Ship

--- sectorfive.rb
+++ sectorfive.rb
@@ -2,9 +2,11 @@ require 'gosu'
 require_relative 'player'
 
 class SectorFive < Gosu::Window
+  WIDTH = 800
+  HEIGHT = 600
   def initialize 
     super(800, 600)
-    self.caption = 'Sector Five!'
+    self.caption = 'Sector Five'
     @player = Player.new(self)
   end
 
@@ -15,6 +17,8 @@ class SectorFive < Gosu::Window
   def update
     @player.turn_left if button_down?(Gosu::KbLeft)
     @player.turn_right if button_down?(Gosu::KbRight)
+    @player.accelerate if button_down?(Gosu::KbUp)
+    @player.move
   end
 end
 

4b80c6f77feabdacb7fc84a24cfa7536e0dca2fb
vbaker007 <vbaker007@hotmail.com>
2016-03-22 11:11:09 -0400

    Create Player Sprite Class & Ship.png Image

--- sectorfive.rb
+++ sectorfive.rb
@@ -1,9 +1,15 @@
 require 'gosu'
+require_relative 'player'
 
 class SectorFive < Gosu::Window
   def initialize 
     super(800, 600)
     self.caption = 'Sector Five!'
+    @player = Player.new(self)
+  end
+
+  def draw
+    @player.draw
   end
 end
 
